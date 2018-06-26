3ef2b2abf7458b5f44408c3b25cdece7c847be4d
gersure <gersure@163.com>
2018-06-27 00:41:47 +0800

    split out

---
+++ paxos-cpp/include/paxos-cpp/version.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define PAXOS-CPP_VERSION_MAJOR 1
+#define PAXOS-CPP_VERSION_MINOR 0
+#define PAXOS-CPP_VERSION_FIXED 0
+#define PAXOS-CPP_GIT_HASH__    
+
+#define PAXOS-CPP_BUILD_TIME__  2018-06-26 16:39
+
+#define _vstr_bind(str) #str
+#define vstr_bind(str) _vstr_bind(str)
+
+#define PAXOS-CPP_GIT_HASH      vstr_bind(PAXOS-CPP_GIT_HASH__)
+#define PAXOS-CPP_BUILD_TIME    vstr_bind(PAXOS-CPP_BUILD_TIME__)
