a29e8c22ef17d14a5b778a678a1a79315d672d12
gersure <gersure@163.com>
2018-06-27 18:49:44 +0800

    add cmake build

--- paxos++/include/paxos-cpp/version.h
+++ paxos++/include/paxos-cpp/version.h
@@ -5,7 +5,7 @@
 #define PAXOS-CPP_VERSION_FIXED 0
 #define PAXOS-CPP_GIT_HASH__    
 
-#define PAXOS-CPP_BUILD_TIME__  2018-06-26 16:52
+#define PAXOS-CPP_BUILD_TIME__  2018-06-27 09:22
 
 #define _vstr_bind(str) #str
 #define vstr_bind(str) _vstr_bind(str)
