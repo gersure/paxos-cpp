7b5166489064fdbf65ce7f630e96dae68dddc519
changfa.zheng <changfa.zheng@ele.me>
2018-06-27 18:49:44 +0800

    add cmake build

---
+++ paxos++/include/paxos++/version.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define PAXOS++_VERSION_MAJOR 1
+#define PAXOS++_VERSION_MINOR 0
+#define PAXOS++_VERSION_FIXED 0
+#define PAXOS++_GIT_HASH__    
+
+#define PAXOS++_BUILD_TIME__  2018-06-27 10:49
+
+#define _vstr_bind(str) #str
+#define vstr_bind(str) _vstr_bind(str)
+
+#define PAXOS++_GIT_HASH      vstr_bind(PAXOS++_GIT_HASH__)
+#define PAXOS++_BUILD_TIME    vstr_bind(PAXOS++_BUILD_TIME__)
