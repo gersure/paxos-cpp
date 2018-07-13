4235f75d01df474406727c55553b997ee7452134
gersure <gersure@163.com>
2018-07-14 02:17:29 +0800

    fix metrics

--- paxos++/metrics/metrics.cpp
+++ paxos++/metrics/metrics.cpp
@@ -3,21 +3,21 @@
 
 Metrics::Metrics()
     : registry_(std::make_shared<Registry>()),
-    paxos_guage_family_(BuildGuage().Name("paxos_guage").Help("paxos guage family").Register(*registry)),
-    paxos_counter_family_(BuildGuage().Name("paxos_counter").Help("paxos counter family").Register(*registry)),
-    paxos_size_histogram_family_(BuildHistogram().Name("paxos_size_hisgogram").Help("paxos value size histogram family").Register(*registry)),
-    paxos_time_histogram_family_(BuildHistogram().Name("paxos_time_hisgogram").Help("paxos method time histogram family").Register(*registry)),
-#define _mk_guage_init(lable, value, ...)  \
-        name##value(paxos_guage_family_.Add({{name, value}})),
-    _make_guage_label(_mk_guage_init)
-#define _mk_counter_init(lable, value, ...)  \
-        name##value(paxos_counter_family_.Add({{name, value}})),
+    paxos_gauge_family_(BuildGauge().Name("paxos_gauge").Help("paxos gauge family").Register(*registry_)),
+    paxos_counter_family_(BuildCounter().Name("paxos_counter").Help("paxos counter family").Register(*registry_)),
+    paxos_size_histogram_family_(BuildHistogram().Name("paxos_size_hisgogram").Help("paxos value size histogram family").Register(*registry_)),
+    paxos_time_histogram_family_(BuildHistogram().Name("paxos_time_hisgogram").Help("paxos method time histogram family").Register(*registry_)),
+#define _mk_gauge_init(label, value, ...)  \
+        gauge_##label##_##value(paxos_gauge_family_.Add({{#label, #value}})),
+    _make_gauge_label(_mk_gauge_init)
+#define _mk_counter_init(label, value, ...)  \
+        counter_##label##_##value(paxos_counter_family_.Add({{#label, #value}})),
     _make_counter_label(_mk_counter_init)
-#define _mk_size_histogram_init(lable, value, ...)  \
-        name##value(paxos_size_histogram_family_.Add({{name, value}}, Histogram::BucketBoundaries({__VA_ARGS__}))),
+#define _mk_size_histogram_init(label, value, ...)  \
+        hsize_##label##_##value(paxos_size_histogram_family_.Add({{#label, #value}}, Histogram::BucketBoundaries({__VA_ARGS__}))),
     _make_size_histogram_label(_mk_size_histogram_init)
-#define _mk_time_histogram_init(lable, value, ...)  \
-        name##value(paxos_time_histogram_family_.Add({{name, value}}, Histogram::BucketBoundaries({__VA_ARGS__}))),
+#define _mk_time_histogram_init(label, value, ...)  \
+        htime_##label##_##value(paxos_time_histogram_family_.Add({{#label, #value}}, Histogram::BucketBoundaries({__VA_ARGS__}))),
     _make_time_histogram_label(_mk_time_histogram_init)
 
     dummy_(0)
