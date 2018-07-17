c3204a5f2ae0cbc011a71ffa4a8f84e5e8761c1b
changfa.zheng <changfa.zheng@ele.me>
2018-07-17 20:20:20 +0800

    checkpoint

--- examples/bench_test/client.cpp
+++ examples/bench_test/client.cpp
@@ -7,9 +7,8 @@
 #include "paxos++/util/timer.hpp"
 
 
-void run_bench(paxos::client& client, std::atomic<long>& count, std::atomic<bool>& end)
+void run_bench(paxos::client& client, std::atomic<long>& count, std::atomic<long>& err_count, std::atomic<bool>& end)
 {
-    long err_count=0;
     while(!end){
         try{
             std::future <std::string> future = client.send ("foo");
@@ -40,7 +39,7 @@ void run_bench(paxos::client& client, std::atomic<long>& count, std::atomic<bool
 int main ()
 {
    paxos::client client;
-   client.add ("127.0.0.1", 1337);
+   client.add ({{"127.0.0.1", 1337},{"127.0.0.1", 1338},{"127.0.0.1", 1339}});
    std::atomic<bool> end(false);
 
    constexpr int con = 10;
@@ -50,18 +49,18 @@ int main ()
 //   std::atomic<long> ato1(1),ato2(1),ato3(1),ato4(1),ato5(1),ato6(1),ato7(1),ato8(1);
 //   std::cout<<"print bench:"<<print_bench(ato1, (ato2), (ato3))<<std::endl;
 
-
-
    std::atomic<long> ato(0);
+   std::atomic<long> ato_err(0);
    for (int i=0; i< con; i++){
-       threads[i] = std::thread(std::bind(run_bench, std::ref(client), std::ref(ato), std::ref(end)));
+       threads[i] = std::thread(std::bind(run_bench, std::ref(client), std::ref(ato), std::ref(ato_err), std::ref(end)));
    }
 
-
+   long total = 0, total_b = 0;
    while(true){
        Timer timer;
        std::this_thread::sleep_for(std::chrono::seconds(2));
-       long total = ato.load();
-       std::cout<<"ops"<<total/((double)timer.elapsed()*1000)<<std::endl;
+       total_b = total;
+       total = ato.load();
+       std::cout<<"\tops:"<<((total-total_b)/(double)timer.elapsed())*1000<<"\terror:"<<ato_err.load()<<std::endl;
    }
 }
